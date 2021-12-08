3902f0c8773c397a41cec5a8be5cf5d349047342
zinhyeok <david9pjh@gmail.com>
2021-12-08 19:12:52 +0900

    Fetch: [aws datetime 확인]

--- 02.py
+++ 02.py
@@ -1,31 +1,5 @@
-import websockets
-import asyncio
-import json
+import time
+import datetime
 
-
-async def upbit_websocket():
-    wb = await websockets.connect(
-        "wss://api.upbit.com/websocket/v1", ping_interval=None
-    )
-    await wb.send(
-        json.dumps(
-            [
-                {"ticket": "test"},
-                {"type": "trade", "codes": ["KRW-BTC", "BTC-BCH", "KRW-AQT"]},
-                {"format": "SIMPLE"},
-            ]
-        )
-    )
-
-    while True:
-        if wb.open:
-            result = await wb.recv()
-            result = json.loads(result)
-            print(result)
-        else:
-            print("연결 안됨! 또는 연결 끊김")
-
-
-loop = asyncio.get_event_loop()
-asyncio.ensure_future(upbit_websocket())
-loop.run_forever()
+now = datetime.datetime.now()
+print(now)
